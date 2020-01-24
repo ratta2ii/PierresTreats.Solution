c0deb7c492b6c753ab06cdd1f701554b86361ea5
Trevor Mackin <tismetrm@gmail.com>
2020-01-24 10:09:45 -0800

    Add Flavor class with auto implemented properties for an Id and Name Implement a many to many relationship with Treats by first adding an ICollection of Treats to the Flavor class Create a Flavor constructor to set the Treats property to a new Hashset with type FlavorTreat

---
+++ PierresTreats/Models/Flavor.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace PierresTreats
+{
+    public class Flavor
+    {
+        public int FlavorId { get; set; }
+        public string FlavorName { get; set; }
+        public virtual ICollection<FlavorTreat> Treats { get; set; }
+        public Flavor()
+        {
+            this.Treats = new HashSet<FlavorTreat>();
+        }
+    }
+}
