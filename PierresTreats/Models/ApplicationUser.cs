4f7aee789bd0f4cd0afd1897df1dc5e69ccf9315
Trevor Mackin <tismetrm@gmail.com>
2020-01-24 11:32:53 -0800

    Correct spelling error

--- PierresTreats/Models/ApplicationUser.cs
+++ PierresTreats/Models/ApplicationUser.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace PierresTreats.Models
 {
-    public class ApplicationUser : IndentityUser
+    public class ApplicationUser : IdentityUser
     {
 
     }
